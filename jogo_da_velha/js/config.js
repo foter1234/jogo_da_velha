6d99e030bff8dadc45fd783a99922922221ab328
Lucas Pereira Esteves <100858060+foter1234@users.noreply.github.com>
2026-04-13 18:39:05 -0300

    up

---
+++ jogo_da_velha/js/config.js
@@ -0,0 +1,4 @@
+
+const SUPABASE_URL = 'https://lfemaaetrqbvmdnvhcoy.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_-HoyrJL7YFeQhgKTggqwdA_jXqKzJZK';
+const SUPA_OK = !SUPABASE_URL.includes('SEU_PROJETO');
